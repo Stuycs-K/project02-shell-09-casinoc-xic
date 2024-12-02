6248bcecfd6e4e55501dbdfd1d7cfac26cc8f95f
Christophe Casino <ccasino50@stuy.edu>
2024-12-02 14:50:34 -0500

    added stuff to main

--- main.c
+++ main.c
@@ -6,3 +6,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+
+int char_counter(char * str){
+  for(int i = 0; i < strlen(str); )
+}
+
+int main(int argc, char *argv[]){
+  char buffer[200];
+  char * ptr = buffer;
+  fgets(ptr, 200, stdin);
+  printf("buffer: %s\n", ptr);
+  return 0;
+}
