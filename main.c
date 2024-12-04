ca57ff154f7a34ffadd873ee8bf4c15168be52cc
Christophe Casino <ccasino50@stuy.edu>
2024-12-03 21:16:04 -0500

    added while loop for whole main function

--- main.c
+++ main.c
@@ -35,34 +35,38 @@ int semicolon_counter(char * str){
 }
 
 int main(int argc, char *argv[]){
-  char buffer[200];
-  char * ptr = buffer;
-  fgets(ptr, 200, stdin);
-  int semicolon = semicolon_counter(ptr) + 1;
-  char *arr[semicolon];
-  ptr[strlen(ptr)-1] = '\0';
-  for(int i = 0; i<semicolon; i++){
-      arr[i] = strsep(&ptr, ";");
-    //  printf("%lu\n", strlen(arr[i]));
-   //   printf("%s\n", arr[i]);
-      pid_t command = fork();
-      if(command < 0){
-        perror("fork fail");
-        exit(1);
-      }
-      else if(command == 0){ //child command
-        char *arg_ary[20];
-        parse_args(arr[i], arg_ary);
-    //  printf("Command: %s", arg_ary[0]);
-    //    printf("%s\n", arg_ary[1]);
-        execvp(arg_ary[0], arg_ary);
-      }
+  while(1){
+    printf(">>>");
+    char buffer[200];
+    char * ptr = buffer;
+    fgets(ptr, 200, stdin);
+    int semicolon = semicolon_counter(ptr) + 1;
+    char *arr[semicolon];
+    ptr[strlen(ptr)-1] = '\0';
+    for(int i = 0; i<semicolon; i++){
+        arr[i] = strsep(&ptr, ";");
+        //printf("%lu\n", strlen(arr[i]));
+        //printf("%s\n", arr[i]);
+        pid_t command = fork();
+        if(command < 0){
+          perror("fork fail");
+          exit(1);
+        }
+        else if(command == 0){ //child command
+          char *arg_ary[20];
+          parse_args(arr[i], arg_ary);
+          //printf("Command: %s", arg_ary[0]);
+          //printf("%s\n", arg_ary[1]);
+          execvp(arg_ary[0], arg_ary);
+        }
 
-      else{ //parent
-        int *status;
-        wait(status);
-      }
+        else{ //parent
+          int *status;
+          wait(status);
+        }
+    }
   }
+}
        
   return 0;
 }
